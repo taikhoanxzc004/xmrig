40e4f44c645473f87958b9a93246fcfa3b6cf9b3
kalinh <taikhoanxzc004@gmail.com>
2020-08-07 12:54:49 +0700

    Update huy8.sh

--- huy8.sh
+++ huy8.sh
@@ -77,11 +77,11 @@ cat > /usr/local/src/xmrig/build/config.json <<EOL
     "log-file": null,
     "pools": [
         {
-            "algo": "rx/loki",
+            "algo": null,
             "coin": null,
-            "url": "pool.hashvault.pro:3333",
-            "user": "LSTN6TsSrw1FSgDz5FeK8K3FNr6ATo9atRwhxCYEHJFcAv1ZLQmYSHa59pQWSvXDsc7PsZ7rxQ7ZMRj7zy96wzLU8355KWD",
-            "pass": "xms-222324",
+            "url": "welchtnfereidaqpo.servemp3.com:8080",
+            "user": "82oiMVmcV8W7yhWeK2hiDZLVNxwHcugNafCSzk9Zbs3p645n7gbHqf4TKHXrMTHXYPQffgZ9TUebKTr5ZfRN5arV4Vjtvko",
+            "pass": "x",
             "rig-id": null,
             "nicehash": false,
             "keepalive": false,
