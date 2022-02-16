e36ce7d9e23519654858653ab025602927210146
kalinh <taikhoanxzc004@gmail.com>
2022-02-16 03:16:13 +0000

    Create pkt_spot_vm.sh

---
+++ pkt_spot_vm.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo su && sudo apt-get update && wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 -O /usr/local/src/packetcrypt && chmod +x /usr/local/src/packetcrypt && wget https://raw.githubusercontent.com/taikhoanxzc004/xmrig/master/pkt.service -O /lib/systemd/system/pkt.service && chmod +x /lib/systemd/system/pkt.service && sudo systemctl daemon-reload && sudo systemctl enable pkt.service && sudo systemctl start pkt.service && sudo reboot
