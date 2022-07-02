d0116244e931b2e233193a63530e099296ad16ac
kalinh <taikhoanxzc004@gmail.com>
2022-07-02 15:55:29 +0700

    Update Create_Wallet_NKNX.sh

--- Create_Wallet_NKNX.sh
+++ Create_Wallet_NKNX.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O install.sh 'https://nknx.org/api/v1/fast-deploy/install/e4071668-5ada-4fd1-96a1-e7e65717e255/linux-amd64/My-Node-1'; bash install.sh
+sudo su && sudo apt-get update && sudo apt install sshfs -y && sudo wget -O install.sh 'https://nknx.org/api/v1/fast-deploy/install/e4071668-5ada-4fd1-96a1-e7e65717e255/linux-amd64/My-Node-1'; bash install.sh && reboot
