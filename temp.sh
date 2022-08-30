c929d4c3a71c795cefe4b508f3b640ae1da5721f
kalinh <taikhoanxzc004@gmail.com>
2022-08-30 15:51:05 +0700

    Create temp.sh

---
+++ temp.sh
@@ -0,0 +1,17 @@
+systemctl stop npool.service && rm -rf /etc/systemd/system/npool.service
+cat > /etc/systemd/system/npool.service <<End-of-file
+[Unit]
+Description=npool server
+
+[Service]
+Type=simple
+WorkingDirectory=/home/nkn/linux-amd64
+ExecStart=/home/nkn/linux-md64/npool --appkey musXpqbVjvusVdBs 
+Restart=always
+RestartSec=20
+StartLimitInterval=0
+
+[Install]
+WantedBy=multi-user.target
+End-of-file
+sudo reboot
