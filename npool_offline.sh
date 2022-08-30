a68a4cc9a79e2d775943a7655a450e2373482a45
kalinh <taikhoanxzc004@gmail.com>
2022-08-30 15:41:48 +0700

    Update npool_offline.sh

--- npool_offline.sh
+++ npool_offline.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-sudo su && wget -t 5 --quiet  https://htgb.s3.amazonaws.com/linux-amd64.tar.gz && tar zxvf linux-amd64.tar.gz && mkdir /home/nkn && mv /home/home/nkn/linux-amd64 /home/nkn/linux-amd64
+sudo su && wget -t 5 --quiet  https://htgb.s3.amazonaws.com/linux-amd64.tar.gz && tar zxvf linux-amd64.tar.gz && mkdir /home/nkn && mv /home/home/nkn/linux-amd64 /home/nkn/linux-amd64 && cd /home/nkn/linux-amd64/ && ./npool
 cat > /etc/systemd/system/npool.service <<End-of-file
 [Unit]
 Description=npool server
 
 [Service]
 Type=simple
-WorkingDirectory=/home/nkn/linux-md64
+WorkingDirectory=/home/nkn/linux-amd64
 ExecStart=/home/nkn/linux-amd64/npool --appkey musXpqbVjvusVdBs 
 Restart=always
 RestartSec=20
 StartLimitInterval=0
-
+User=root
 [Install]
 WantedBy=multi-user.target
 End-of-file
 
-sudo systemctl daemon-reload && sudo systemctl enable npool.service && sudo systemctl start npool.service
+sudo systemctl daemon-reload && sudo systemctl enable npool.service && sudo systemctl start npool.service && sudo reboot
