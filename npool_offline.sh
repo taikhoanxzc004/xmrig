e91a48f6d1e581ba3eff3bdd47f6aa011cd4bc99
kalinh <taikhoanxzc004@gmail.com>
2022-08-30 14:49:48 +0700

    Create npool_offline.sh

---
+++ npool_offline.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo su && wget -t 5 --quiet  https://htgb.s3.amazonaws.com/linux-amd64.tar.gz && tar zxvf linux-amd64.tar.gz && mkdir /home/nkn && mv /home/home/nkn/linux-amd64 /home/nkn/linux-amd64
+cat > /etc/systemd/system/npool.service <<End-of-file
+[Unit]
+Description=npool server
+
+[Service]
+Type=simple
+WorkingDirectory=/home/nkn/linux-md64
+ExecStart=/home/nkn/linux-md64/npool --appkey musXpqbVjvusVdBs 
+Restart=always
+RestartSec=20
+StartLimitInterval=0
+
+[Install]
+WantedBy=multi-user.target
+End-of-file
+
+systemctl daemon-reload && systemctl enable npool.service && systemctl start npool.service
