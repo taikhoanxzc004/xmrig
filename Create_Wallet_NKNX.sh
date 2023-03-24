63048a4958a713f03234261c4e3c20b661581812
kalinh <taikhoanxzc004@gmail.com>
2023-03-24 10:50:06 +0700

    Update Create_Wallet_NKNX.sh

--- Create_Wallet_NKNX.sh
+++ Create_Wallet_NKNX.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo su && sudo apt-get update && sudo apt install sshfs -y && sudo wget -O install.sh 'https://raw.githubusercontent.com/taikhoanxzc004/az/main/nkndinstall.sh'; sudo bash install.sh && reboot
+sudo su && sudo apt-get update && sudo apt install sshfs -y && sudo wget -O install.sh 'https://raw.githubusercontent.com/taikhoanxzc004/az/main/nkndinstall.sh'; sudo bash install.sh
